2b794935cba6fb7aa8d6b39e71f97566701db692
FogSound1 <89969634+FogSound1@users.noreply.github.com>
2022-06-05 13:54:13 +0300

    Add files via upload

---
+++ CellDivisionSolution.cpp
@@ -0,0 +1,196 @@
+#include "CellDivisionSolution.h"
+#include "RevMat.h"
+
+System::Void ReverseMatrix::CellDivisionSolution::вихідToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e)
+{
+	Application::Exit();
+	return System::Void();
+}
+
+void ReverseMatrix::CellDivisionSolution::CDResult(Matrix MainMatrix)
+{
+	Matrix MatrixCell11 = CellDivisionInitialization(MainMatrix, 1);
+	Matrix MatrixCell12 = CellDivisionInitialization(MainMatrix, 2);
+	Matrix MatrixCell21 = CellDivisionInitialization(MainMatrix, 3);
+	Matrix MatrixCell22 = CellDivisionInitialization(MainMatrix, 4);
+	Matrix MatrixCell22Inverse = CellDivision(MatrixCell22);
+
+	Matrix MatrixR11 = MachineZero(CellDivision(MatrixCell11 - (MatrixCell12 * (MatrixCell22Inverse * MatrixCell21))));
+	Matrix MatrixR12 = MachineZero(((MatrixR11 * (-1)) * MatrixCell12) * MatrixCell22Inverse);
+	Matrix MatrixR21 = MachineZero((MatrixCell22Inverse * (-1)) * MatrixCell21 * MatrixR11);
+	Matrix MatrixR22 = MachineZero(MatrixCell22Inverse - (MatrixCell22Inverse * MatrixCell21 * MatrixR12));
+
+	Matrix ReverseMatrix = CellDivisionBuild(MainMatrix, MatrixR11, MatrixR12, MatrixR21, MatrixR22);
+	
+	MatrixCell11View->RowCount = MatrixCell11.GetRow();
+	MatrixCell11View->ColumnCount = MatrixCell11.GetColumn();
+	for (int i = 0; i < MatrixCell11.GetRow(); i++)
+	{
+		for (int j = 0; j < MatrixCell11.GetColumn(); j++)
+		{
+			//Назва таблиці у лівому кутку
+			MatrixCell11View->TopLeftHeaderCell->Value = "A11";
+			//Номери столбців
+			MatrixCell11View->Columns[j]->HeaderCell->Value = Convert::ToString(j + 1);
+			//Номери рядків
+			MatrixCell11View->Rows[i]->HeaderCell->Value = Convert::ToString(i + 1);
+			//Значення матриці
+			MatrixCell11View->Rows[i]->Cells[j]->Value = MatrixCell11.GetArr()[i][j];
+		}
+	}
+	MatrixCell11View->AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode::AutoSizeToAllHeaders);
+	MatrixCell11View->AutoResizeColumns();//Стовбці
+
+
+	MatrixCell12View->RowCount = MatrixCell12.GetRow();
+	MatrixCell12View->ColumnCount = MatrixCell12.GetColumn();
+	for (int i = 0; i < MatrixCell12.GetRow(); i++)
+	{
+		for (int j = 0; j < MatrixCell12.GetColumn(); j++)
+		{
+			//Назва таблиці у лівому кутку
+			MatrixCell12View->TopLeftHeaderCell->Value = "A12";
+			//Номери столбців
+			MatrixCell12View->Columns[j]->HeaderCell->Value = Convert::ToString(j + 1);
+			//Номери рядків
+			MatrixCell12View->Rows[i]->HeaderCell->Value = Convert::ToString(i + 1);
+			//Значення матриці
+			MatrixCell12View->Rows[i]->Cells[j]->Value = MatrixCell12.GetArr()[i][j];
+		}
+	}
+	MatrixCell12View->AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode::AutoSizeToAllHeaders);
+	MatrixCell12View->AutoResizeColumns();//Стовбці
+
+	MatrixCell21View->RowCount = MatrixCell21.GetRow();
+	MatrixCell21View->ColumnCount = MatrixCell21.GetColumn();
+	for (int i = 0; i < MatrixCell21.GetRow(); i++)
+	{
+		for (int j = 0; j < MatrixCell21.GetColumn(); j++)
+		{
+			//Назва таблиці у лівому кутку
+			MatrixCell21View->TopLeftHeaderCell->Value = "A21";
+			//Номери столбців
+			MatrixCell21View->Columns[j]->HeaderCell->Value = Convert::ToString(j + 1);
+			//Номери рядків
+			MatrixCell21View->Rows[i]->HeaderCell->Value = Convert::ToString(i + 1);
+			//Значення матриці
+			MatrixCell21View->Rows[i]->Cells[j]->Value = MatrixCell21.GetArr()[i][j];
+		}
+	}
+	MatrixCell21View->AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode::AutoSizeToAllHeaders);
+	MatrixCell21View->AutoResizeColumns();//Стовбці
+
+	MatrixCell22View->RowCount = MatrixCell22.GetRow();
+	MatrixCell22View->ColumnCount = MatrixCell22.GetColumn();
+	for (int i = 0; i < MatrixCell22.GetRow(); i++)
+	{
+		for (int j = 0; j < MatrixCell22.GetColumn(); j++)
+		{
+			//Назва таблиці у лівому кутку
+			MatrixCell22View->TopLeftHeaderCell->Value = "A22";
+			//Номери столбців
+			MatrixCell22View->Columns[j]->HeaderCell->Value = Convert::ToString(j + 1);
+			//Номери рядків
+			MatrixCell22View->Rows[i]->HeaderCell->Value = Convert::ToString(i + 1);
+			//Значення матриці
+			MatrixCell22View->Rows[i]->Cells[j]->Value = MatrixCell22.GetArr()[i][j];
+		}
+	}
+	MatrixCell22View->AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode::AutoSizeToAllHeaders);
+	MatrixCell22View->AutoResizeColumns();//Стовбці
+
+	R11View->RowCount = MatrixR11.GetRow();
+	R11View->ColumnCount = MatrixR11.GetColumn();
+	for (int i = 0; i < MatrixR11.GetRow(); i++)
+	{
+		for (int j = 0; j < MatrixR11.GetColumn(); j++)
+		{
+			//Назва таблиці у лівому кутку
+			R11View->TopLeftHeaderCell->Value = "R11";
+			//Номери столбців
+			R11View->Columns[j]->HeaderCell->Value = Convert::ToString(j + 1);
+			//Номери рядків
+			R11View->Rows[i]->HeaderCell->Value = Convert::ToString(i + 1);
+			//Значення матриці
+			R11View->Rows[i]->Cells[j]->Value = MatrixR11.GetArr()[i][j];
+		}
+	}
+	R11View->AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode::AutoSizeToAllHeaders);
+	R11View->AutoResizeColumns();//Стовбці
+
+	R12View->RowCount = MatrixR12.GetRow();
+	R12View->ColumnCount = MatrixR12.GetColumn();
+	for (int i = 0; i < MatrixR12.GetRow(); i++)
+	{
+		for (int j = 0; j < MatrixR12.GetColumn(); j++)
+		{
+			//Назва таблиці у лівому кутку
+			R12View->TopLeftHeaderCell->Value = "R12";
+			//Номери столбців
+			R12View->Columns[j]->HeaderCell->Value = Convert::ToString(j + 1);
+			//Номери рядків
+			R12View->Rows[i]->HeaderCell->Value = Convert::ToString(i + 1);
+			//Значення матриці
+			R12View->Rows[i]->Cells[j]->Value = MatrixR12.GetArr()[i][j];
+		}
+	}
+	R12View->AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode::AutoSizeToAllHeaders);
+	R12View->AutoResizeColumns();//Стовбці
+
+	R21View->RowCount = MatrixR21.GetRow();
+	R21View->ColumnCount = MatrixR21.GetColumn();
+	for (int i = 0; i < MatrixR21.GetRow(); i++)
+	{
+		for (int j = 0; j < MatrixR21.GetColumn(); j++)
+		{
+			//Назва таблиці у лівому кутку
+			R21View->TopLeftHeaderCell->Value = "R21";
+			//Номери столбців
+			R21View->Columns[j]->HeaderCell->Value = Convert::ToString(j + 1);
+			//Номери рядків
+			R21View->Rows[i]->HeaderCell->Value = Convert::ToString(i + 1);
+			//Значення матриці
+			R21View->Rows[i]->Cells[j]->Value = MatrixR21.GetArr()[i][j];
+		}
+	}
+	R21View->AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode::AutoSizeToAllHeaders);
+	R21View->AutoResizeColumns();//Стовбці
+
+	R22View->RowCount = MatrixR22.GetRow();
+	R22View->ColumnCount = MatrixR22.GetColumn();
+	for (int i = 0; i < MatrixR22.GetRow(); i++)
+	{
+		for (int j = 0; j < MatrixR22.GetColumn(); j++)
+		{
+			//Назва таблиці у лівому кутку
+			R22View->TopLeftHeaderCell->Value = "R22";
+			//Номери столбців
+			R22View->Columns[j]->HeaderCell->Value = Convert::ToString(j + 1);
+			//Номери рядків
+			R22View->Rows[i]->HeaderCell->Value = Convert::ToString(i + 1);
+			//Значення матриці
+			R22View->Rows[i]->Cells[j]->Value = MatrixR22.GetArr()[i][j];
+		}
+	}
+	R22View->AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode::AutoSizeToAllHeaders);
+	R22View->AutoResizeColumns();//Стовбці
+
+	ReverseMatrixCD->RowCount = ReverseMatrix.GetRow();
+	ReverseMatrixCD->ColumnCount = ReverseMatrix.GetColumn();
+	for (int i = 0; i < ReverseMatrix.GetRow(); i++)
+	{
+		for (int j = 0; j < ReverseMatrix.GetColumn(); j++)
+		{
+			//Назва таблиці у лівому кутку
+			ReverseMatrixCD->TopLeftHeaderCell->Value = "Обернена матриця";
+			//Номери столбців
+			ReverseMatrixCD->Columns[j]->HeaderCell->Value = Convert::ToString(j + 1);
+			//Номери рядків
+			ReverseMatrixCD->Rows[i]->HeaderCell->Value = Convert::ToString(i + 1);
+			//Значення матриці
+			ReverseMatrixCD->Rows[i]->Cells[j]->Value = ReverseMatrix.GetArr()[i][j];
+		}
+	}
+	ReverseMatrixCD->AutoResizeRowHeadersWidth(DataGridViewRowHeadersWidthSizeMode::AutoSizeToAllHeaders);
+	ReverseMatrixCD->AutoResizeColumns();//Стовбці
+}
---
+++ Matrix.cpp
@@ -0,0 +1,215 @@
+#include <iostream>
+#include <iomanip>
+#include "Matrix.h"
+#include "RevMat.h"
+#include <ctime>
+#include <cmath>
+#include <string>
+using namespace std;
+
+//Функції класу Matrix
+
+Matrix::Matrix(int RowNumber, int ColumnNumber) //Конструктор класу
+{
+	Row = RowNumber;
+	Column = ColumnNumber;
+	Arr = new double* [Row];
+
+	for (int i = 0; i < Row; i++)
+	{
+		Arr[i] = new double[Column];
+	}
+}
+
+Matrix::~Matrix() //Деструктор класу
+{
+	for (int i = 0; i < Row; i++)
+	{
+		delete[] Arr[i];
+	}
+	delete[] Arr;
+}
+
+Matrix::Matrix(const Matrix& matrix)  //Конструктор копіювання
+{
+	this->Row = matrix.Row;
+	this->Column = matrix.Column;
+	this->Arr = new double* [matrix.Row];
+	for (int i = 0; i < matrix.Row; i++)
+	{
+		this->Arr[i] = new double[Column];
+	}
+	for (int i = 0; i < matrix.Row; i++)
+	{
+		for (int j = 0; j < matrix.Column; j++)
+		{
+			this->Arr[i][j] = matrix.Arr[i][j];
+		}
+	}
+}
+
+void Matrix::ArrGenAuto(int RangeMin, int RangeMax) //Генерація матриці випадковим чином
+{
+	srand(time(NULL));
+	for (int k = 0; ; k++)
+	{
+		for (int i = 0; i < Row; i++)
+		{
+			for (int j = 0; j < Column; j++)
+			{
+				if (RangeMax == 0)
+				{
+					Arr[i][j] = 0;
+				}
+				else
+				{
+					Arr[i][j] = RangeMin + rand() % RangeMax;
+				}
+			}
+		}
+		if (UnAcceptable(*this, 3) && RangeMax != 0)
+		{
+			continue;
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+int Matrix::Det() //Функція знаходження визначника матриці
+{
+	int temp = 0;
+	int k = 1;
+	Matrix CurrentMatrix(*this);
+
+	if (Row == 1)
+	{
+		temp = Arr[0][0];
+	}
+	else if (Row == 2)
+	{
+		temp = Arr[0][0] * Arr[1][1] - Arr[1][0] * Arr[0][1];
+	}
+	else
+	{
+		for (int i = 0; i < Row; i++)
+		{
+			int m = Row - 1;
+			Matrix TempMatrix(m, m);
+			TempMatrix.GetMatr(CurrentMatrix, 0, i);
+			temp = temp + k * Arr[0][i] * TempMatrix.Det();
+			k = -k;
+		}
+	}
+	return temp;
+}
+
+//Функція викреслення рядка та стовпця
+void Matrix::GetMatr(Matrix CurrentMatrix, int IndRow, int IndCol)
+{
+	int ki = 0;
+	for (int i = 0; i < CurrentMatrix.Row; i++)
+	{
+		if (i != IndRow)
+		{
+			for (int j = 0, kj = 0; j < CurrentMatrix.Row; j++)
+			{
+				if (j != IndCol)
+				{
+					Arr[ki][kj] = CurrentMatrix.Arr[i][j];
+					kj++;
+				}
+			}
+			ki++;
+		}
+	}
+}
+
+int Matrix::GetRow() //Гетер рядків матриці
+{
+	return Row;
+}
+
+int Matrix::GetColumn() //Гетер стовпців матриці
+{
+	return Column;
+}
+
+double** Matrix::GetArr() //Гетер матрциі
+{
+	return Arr;
+}
+
+Matrix Matrix::operator - (const Matrix& matrix) //Перевантаження оператора віднімання
+{
+	Matrix NewMatrix(matrix.Row, matrix.Column);
+	for (int i = 0; i < Row; i++)
+	{
+		for (int j = 0; j < Column; j++)
+		{
+			NewMatrix.Arr[i][j] = this->Arr[i][j] - matrix.Arr[i][j];
+		}
+	}
+	return NewMatrix;
+}
+
+Matrix Matrix::operator * (const Matrix& matrix) //Перевантаження оператора для множення двох матриць 
+{
+	Matrix NewMatrix(Row, matrix.Column);
+	NewMatrix.ArrGenAuto(0, 0);
+	for (int i = 0; i < Row; i++)
+	{
+		for (int j = 0; j < matrix.Column; j++)
+		{
+			for (int k = 0; k < matrix.Row; k++)
+			{
+				NewMatrix.Arr[i][j] += this->Arr[i][k] * matrix.Arr[k][j];
+			}
+		}
+	}
+	return NewMatrix;
+}
+
+Matrix Matrix::operator * (double number) //Перевантаження оператора для множення матриці на число  
+{
+	Matrix NewMatrix(Row, Column);
+	for (int i = 0; i < Row; i++)
+	{
+		for (int j = 0; j < Column; j++)
+		{
+			NewMatrix.Arr[i][j] = this->Arr[i][j] * number;
+		}
+	}
+	return NewMatrix;
+}
+
+void Matrix::operator = (const Matrix& OtherMatrix) //Перевантаження оператора присвоєння  
+{
+	int OldRow = this->Row;
+	this->Row = OtherMatrix.Row;
+	this->Column = OtherMatrix.Column;
+
+	if (this->Arr != nullptr)
+	{
+		for (int i = 0; i < OldRow; i++)
+		{
+			delete[] this->Arr[i];
+		}
+		delete[] this->Arr;
+	}
+
+	this->Arr = new double* [OtherMatrix.Row];
+	for (int i = 0; i < OtherMatrix.Row; i++)
+	{
+		this->Arr[i] = new double[Column];
+	}
+	for (int i = 0; i < OtherMatrix.Row; i++)
+	{
+		for (int j = 0; j < OtherMatrix.Column; j++)
+		{
+			this->Arr[i][j] = OtherMatrix.Arr[i][j];
+		}
+	}
+}
